ac286537805154927532b5fd01593148d2f23f0b
Dmitry Tolmachov <d.tolmachov@aimprosoft.com>
2018-03-05 13:01:24 +0200

    small changes

--- wowViewerLib/src/engine/managers/particles/CRndSeed.cpp
+++ wowViewerLib/src/engine/managers/particles/CRndSeed.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <random>
+#include <assert.h>
 #include "CRndSeed.h"
 
 uint32_t CRndSeed::uint32t() {
@@ -22,27 +23,35 @@ float CRndSeed::Uniform() {
     uint32_t u = this->uint32t();
     // [1, 2)
     fi.i = 0x3f800000 | (0x7fffff & u);
+    float result;
     if (u & 0x80000000) {
-        return 2.0f - fi.f;
+        result = 2.0f - fi.f;
     } else {
-        return fi.f - 2.0f;
+        result = fi.f - 2.0f;
     }
-//    return (float) (drand48() * 2.0f - 1.0f);
+    result = (float) (drand48() * 2.0f - 1.0f);
+
+    assert(result < 1.0 || result > -1.0);
+    return result;
 }
 
 float CRndSeed::UniformPos() {
-//    union {
-//        float f;
-//        uint32_t i;
-//    } fi;
-//    uint32_t u = this->uint32t();
-//    // [1, 2)
-//    fi.i = 0x3f800000 | (0x7fffff & u);
-//    if (u & 0x80000000) {
-//        return 1.0f - fi.f;
-//    } else {
-//        return fi.f - 1.0f;
-//    }
+    union {
+        float f;
+        uint32_t i;
+    } fi;
+    uint32_t u = this->uint32t();
+    // [1, 2)
+    fi.i = 0x3f800000 | (0x7fffff & u);
+    float result;
+    if (u & 0x80000000) {
+        result = 1.0f - fi.f;
+    } else {
+        result =  fi.f - 1.0f;
+    }
 
-    return (float) drand48();
+    result = (float) drand48();
+
+    assert(result > 0.0 || result < 1.0);
+    return result;
 }
