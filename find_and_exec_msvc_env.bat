2cda6ab2f86b05090b24125747a52e5c62c44825
Deamon87 <dmitolm@gmail.com>
2025-03-29 22:43:03 +0200

    fix

--- find_and_exec_msvc_env.bat
+++ find_and_exec_msvc_env.bat
@@ -1,4 +1,5 @@
 @echo off
+rem This file finds and adds the msvc values to env
 
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
   set InstallDir=%%i
