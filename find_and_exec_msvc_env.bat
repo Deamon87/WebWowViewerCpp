503f440dea3983753b9324ff3e47f3239ea79183
Deamon87 <dmitolm@gmail.com>
2025-03-30 19:25:56 +0300

    test

--- find_and_exec_msvc_env.bat
+++ find_and_exec_msvc_env.bat
@@ -1,5 +1,5 @@
 @echo off
-rem This file finds and adds the msvc values to env
+@rem This file finds and adds the msvc values to env
 
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
   set InstallDir=%%i
@@ -9,6 +9,6 @@ if exist "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" (
   call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" amd64
   @echo on
   echo "Exporting PATH to path.txt"
-  echo %Path% > path.txt
+@REM   echo %Path% > path.txt
   echo "Exporting finished"
 )
