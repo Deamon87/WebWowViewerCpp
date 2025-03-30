4b51333e54e5a29c0c050d2c83dbb4d79a46182b
Deamon87 <dmitolm@gmail.com>
2025-03-31 00:25:14 +0300

    test

--- find_and_exec_msvc_env.bat
+++ find_and_exec_msvc_env.bat
@@ -9,6 +9,6 @@ if exist "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" (
   call "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" amd64
   @echo on
   echo "Exporting PATH to ADD_PATH_ENV"
-  echo "ADD_PATH_ENV=%PATH%" | Out-File -FilePath $env:GITHUB_ENV -Append
+  echo "ADD_PATH_ENV=%PATH%" > $env:GITHUB_ENV
   echo "Exporting finished"
 )
